721d96f3dd8aa4fbedb4d3bc509ff2ef6032fe82
Ankit <ankitdj13112002@gmail.com>
2023-08-04 00:31:47 +0530

    header completed

---
+++ frontend/src/components/featured/Featured.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./featured.css";
+const Featured = () => {
+  return <div>featured</div>;
+};
+
+export default Featured;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
-
+import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
+import "./home.css";
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </div>
   );
 };
